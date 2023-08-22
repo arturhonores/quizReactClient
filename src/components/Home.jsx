4f13eb295703cc6e5eb11848fefbad2886f280d2
arturhonores <luis.arturhonores@gmail.com>
2023-08-22 02:04:07 +0200

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react"
 import { useDispatch } from "react-redux"
 import { Link } from "react-router-dom"
-import logoJavascript from "/js-icon.png"
+import logoReact from "/reactLogo.svg"
 import { setUserId } from "../redux/result_reducer"
 
 const Home = () => {
@@ -18,7 +18,7 @@ const Home = () => {
         <div className="text-white my-10 mx-auto p-5 max-w-3xl flex flex-col gap-6">
             <div className="border-4 border-spacing-1 border-yellow-500 py-4 flex justify-center items-center gap-2">
                 <h1 className="text-2xl text-center uppercase font-bold">Desafío</h1>
-                <img src={logoJavascript} alt="" width="40px" />
+                <img src={logoReact} alt="" width="40px" />
             </div>
             <ol className="list-decimal list-inside">
                 <li>Se te harán 12 preguntas una tras otra.</li>
